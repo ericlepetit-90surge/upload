86e8ab609a4effb5de6608e3a49676b3e195da3f
ericlepetit-90surge <ericlepetit@gmail.com>
2025-07-27 09:14:19 -0400

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -15,15 +15,9 @@ const oauth2Client = new google.auth.OAuth2(
   oauthClient.web.redirect_uris[0]
 );
 
-<<<<<<< HEAD
-const tokenData = JSON.parse(process.env.GOOGLE_TOKEN_JSON);
-oauth2Client.setCredentials(tokenData);
-=======
 const tokenPath = path.join(process.cwd(), 'GOOGLE_TOKEN.json'); // ✅ Local file
 const token = JSON.parse(fs.readFileSync(tokenPath, 'utf8'));
 oauth2Client.setCredentials(token);
-
->>>>>>> 5b81ac3 (latest version - works locally)
 
 const drive = google.drive({ version: 'v3', auth: oauth2Client });
 
